4948cef98d3e9ed01efe97dff47fe6478cf81545
Fernando Ramirez <86069304+ferrami@users.noreply.github.com>
2023-07-02 04:09:52 +0000

    completed

--- src/models.py
+++ src/models.py
@@ -7,23 +7,86 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_name = Column(String(250), nullable=False)
+    first_name = Column(String(250), nullable=False)
+    last_name = Column(String(250))
+    email = Column(String(250), nullable=False)
+    bio = Column(String(250))
+    followers = Column(String(250))
+    followed = Column(String(250))
+    url = Column(String(250))
+    profile_picture = Column(String(250))
 
-class Address(Base):
-    __tablename__ = 'address'
+class Followers(Base):
+    __tablename__ = 'followers'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_from_id = Column(Integer, ForeignKey('user.id'))
+    user_to_id = Column(Integer, ForeignKey('user.id'))
+    hour = Column(Integer)
+    date = Column(Integer)
+
+    def to_dict(self):
+        return {}
+
+class Post(Base):
+    __tablename__ = 'post'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    hour = Column(Integer)
+    date = Column(Integer)
+    likes = Column(Integer)
+    comment_id = Column(Integer, ForeignKey('comment.id'))
+    sharings = Column(Integer)
+
+    def to_dict(self):
+        return {}
+    
+class Comment(Base):
+    __tablename__ = 'comment'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    comment_text = Column(String(250))
+
+    def to_dict(self):
+        return {}
+
+class Media(Base):
+    __tablename__ = 'media'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    type = Column(String(250))
+    url = Column(String(250))
+
+    def to_dict(self):
+        return {}
+    
+class Hashtags(Base):
+    __tablename__ = 'hashtags'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    hour = Column(Integer)
+    date = Column(Integer)
+    url = Column(String(250))
+    label_name = Column(String(250))
+    comment_id = Column(Integer, ForeignKey('comment.id'))
 
     def to_dict(self):
         return {}
